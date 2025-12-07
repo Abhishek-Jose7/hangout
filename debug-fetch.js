8cc05397eda87b956e18213b3e1b3af3da8e43f4
Abhishek-Jose7 <abhishekjose780@gmail.com>
2025-12-07 21:31:52 +0530

    h

--- debug-fetch.js
+++
@@ -1,6 +0,0 @@
-fetch('https://hqdudeuoapouypiadogv.supabase.co')
-    .then(res => console.log('Status:', res.status))
-    .catch(err => {
-        console.error('Fetch error:', err.message);
-        if (err.cause) console.error('Cause:', err.cause);
-    });
